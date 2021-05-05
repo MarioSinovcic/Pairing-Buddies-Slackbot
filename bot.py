55497024ad6a51d13805fad992462bf65b729c34
Roger Smith <roger.smith@myob.com>
2021-05-05 15:31:23 +1200

    Refactor bot code and add static type checking

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 import boto3
 from slack_bolt import App
 from secrets_retriever import SecretsRetriever
+from typing import List, Dict
 
 SINGLE_USER = "You're the only pear on this tree. Best find more fruit friends to pear with!\n(The pearing bot has only found 1 human user in this channel, feel free to remove it if not in use)."
 MESSAGE_HEADER = "You hear a rustling from the fruit bowl nearby. A lone pear within declares: \n\n"
@@ -27,38 +28,38 @@ class SlackBot:
             signing_secret=self.slack_signing_secret
         )
 
-    def run(self):
-        user_ids = self.get_user_ids() 
-        print(user_ids)
+    def run(self) -> None:
+        user_ids = self.get_human_user_ids() 
         shuffled_ids = self.shuffle_user_ids(user_ids=user_ids)
-        print("user ids shuffled")
         self.post_to_channel(user_ids=user_ids)
 
-    def get_user_ids(self):
+    def get_human_user_ids(self) -> List[str]:
+        user_ids = self.get_user_ids_from_channel()
+        user_ids = self.remove_bots_from_user_ids(user_ids)
+        test_names = ["Ashley", "Tom", "Lindsay", "Alvin", "Leah", "Taylor", "Chrisna", "Sam"]
+        user_ids = user_ids + test_names
+        return user_ids
+
+    def get_user_ids_from_channel(self) -> Dict[str, str]:
         response = self.app.client.conversations_members(channel=self.CHANNEL_ID)
-        user_ids = response["members"]
-        print("user ids gotten")
-        
+        return response['members']
+
+    def remove_bots_from_user_ids(self, user_ids) -> List[str]:
         for user_id in user_ids:
             response = self.app.client.users_info(user=user_id)
             is_bot = response["user"]["is_bot"]
             if is_bot:
                 user_ids.remove(user_id)
-
-        user_ids += ["Ashley", "Tom", "Lindsay", "Alvin", "Leah", "Taylor", "Chrisna", "Sam"]
         return user_ids
-
-    def verify_user_is_human(self):
-        pass
     
-    def shuffle_user_ids(self, user_ids: list):
+    def shuffle_user_ids(self, user_ids: list) -> List[str]:
         shuffled_ids = random.shuffle(user_ids)
         return shuffled_ids
 
-    def mention_user(self, user_id):
+    def generate_user_mention_tag(self, user_id) -> str:
         return "<@" + user_id + ">"
     
-    def format_user_ids_into_user_string(self, user_ids):
+    def format_user_ids_into_user_string(self, user_ids) -> str:
         member_string = ""
 
         if len(user_ids) == 0 or not user_ids:
@@ -68,17 +69,15 @@ class SlackBot:
         elif len(user_ids) % 2 == 0:
             member_string += MESSAGE_HEADER
             for x in range(0, len(user_ids), 2):
-                member_string += self.mention_user(user_ids[x]) + " pears with " + self.mention_user(user_ids[x + 1]) + "\n"
+                member_string += self.generate_user_mention_tag(user_ids[x]) + " pears with " + self.generate_user_mention_tag(user_ids[x + 1]) + "\n"
         else:
             member_string += MESSAGE_HEADER
-            member_string += self.mention_user(user_ids[0]) + " pears with " 
+            member_string += self.generate_user_mention_tag(user_ids[0]) + " pears with " 
             for x in range(1, len(user_ids), 2):
-                member_string += self.mention_user(user_ids[x]) + " pears with " + self.mention_user(user_ids[x + 1]) + "\n"
-        print("user list created")
+                member_string += self.generate_user_mention_tag(user_ids[x]) + " pears with " + self.generate_user_mention_tag(user_ids[x + 1]) + "\n"
+
         return member_string
 
-    def post_to_channel(self, user_ids):
-        print("post to channel attempted")
+    def post_to_channel(self, user_ids) -> None:
         user_string = self.format_user_ids_into_user_string(user_ids=user_ids)
         self.app.client.chat_postMessage(channel=self.CHANNEL_ID, text=user_string)
-        print("post to channel active")
